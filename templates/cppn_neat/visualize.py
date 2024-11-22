e492862ade3941fbba1b9d83ed7d03c011b895af
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2024-11-22 16:19:44 +0000

    reformat visualize.py

--- templates/cppn_neat/visualize.py
+++ templates/cppn_neat/visualize.py
@@ -18,26 +18,29 @@ Code adapted from https://github.com/EvolutionGym/evogym/blob/de71e9a30fe029ee10
 
 
 def parse_range(str_inp, rbt_max):
-    
     inp_with_spaces = ""
     out = []
-    
+
     for token in str_inp:
         if token == "-":
             inp_with_spaces += " " + token + " "
         else:
             inp_with_spaces += token
-    
+
     tokens = inp_with_spaces.split()
 
     count = 0
     while count < len(tokens):
-        if (count+1) < len(tokens) and tokens[count].isnumeric() and tokens[count+1] == "-":
+        if (
+            (count + 1) < len(tokens)
+            and tokens[count].isnumeric()
+            and tokens[count + 1] == "-"
+        ):
             curr = tokens[count]
             last = rbt_max
-            if (count+2) < len(tokens) and tokens[count+2].isnumeric():
-                last = tokens[count+2]
-            for i in range (int(curr), int(last)+1):
+            if (count + 2) < len(tokens) and tokens[count + 2].isnumeric():
+                last = tokens[count + 2]
+            for i in range(int(curr), int(last) + 1):
                 out.append(i)
             count += 3
         else:
@@ -46,21 +49,21 @@ def parse_range(str_inp, rbt_max):
             count += 1
     return out
 
-def pretty_print(list_org, max_name_length=30):
 
+def pretty_print(list_org, max_name_length=30):
     list_formatted = []
-    for i in range(len(list_org)//4 +1):
+    for i in range(len(list_org) // 4 + 1):
         list_formatted.append([])
 
     for i in range(len(list_org)):
-        row = i%(len(list_org)//4 +1)
+        row = i % (len(list_org) // 4 + 1)
         list_formatted[row].append(list_org[i])
 
     print()
     for row in list_formatted:
         out = ""
         for el in row:
-            out += str(el) + " "*(max_name_length - len(str(el)))
+            out += str(el) + " " * (max_name_length - len(str(el)))
         print(out)
 
 
@@ -76,6 +79,7 @@ def get_generations(load_dir, exp_name):
         gen_count += 1
     return [i for i in range(gen_count)]
 
+
 def get_exp_gen_data(exp_name, load_dir, gen):
     robot_data = []
     gen_data_path = os.path.join(load_dir, exp_name, f"generation_{gen}", "output.txt")
@@ -83,6 +87,7 @@ def get_exp_gen_data(exp_name, load_dir, gen):
     for line in f:
         robot_data.append((int(line.split()[0]), float(line.split()[1])))
     return robot_data
+
 
 def save_robot_gif(out_path, env_name, body_path, ctrl_path, seed=42):
     global GIF_RESOLUTION
@@ -92,25 +97,30 @@ def save_robot_gif(out_path, env_name, body_path, ctrl_path, seed=42):
     for key, value in structure_data.items():
         structure.append(value)
     structure = tuple(structure)
-    
+
     model = PPO.load(ctrl_path)
-    
+
     # Parallel environments
-    vec_env = make_vec_env(env_name, n_envs=1, seed=seed, env_kwargs={
-        'body': structure[0],
-        'connections': structure[1],
-        "render_mode": "img",
-    })
-    
+    vec_env = make_vec_env(
+        env_name,
+        n_envs=1,
+        seed=seed,
+        env_kwargs={
+            "body": structure[0],
+            "connections": structure[1],
+            "render_mode": "img",
+        },
+    )
+
     obs = vec_env.reset()
-    imgs = [vec_env.env_method('render')[0]] # vec env is stupid; .render() dosent work
+    imgs = [vec_env.env_method("render")[0]]  # vec env is stupid; .render() dosent work
     done = False
     while not done:
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, done, info = vec_env.step(action)
-        imgs.append(vec_env.env_method('render')[0])
+        imgs.append(vec_env.env_method("render")[0])
 
-    imageio.mimsave(f'{out_path}.gif', imgs, duration=(1/50.0))
+    imageio.mimsave(f"{out_path}.gif", imgs, duration=(1 / 50.0))
     try:
         optimize(out_path)
     except:
@@ -118,46 +128,50 @@ def save_robot_gif(out_path, env_name, body_path, ctrl_path, seed=42):
         # print("Error optimizing gif. Most likely cause is that gifsicle is not installed.")
     return 0
 
-class Robot():
+
+class Robot:
     def __init__(
-        self, 
-        body_path=None, 
-        ctrl_path=None, 
-        reward=None, 
-        env_name=None, 
-        exp_name=None, 
-        gen=None):
+        self,
+        body_path=None,
+        ctrl_path=None,
+        reward=None,
+        env_name=None,
+        exp_name=None,
+        gen=None,
+    ):
         self.body_path = body_path
         self.ctrl_path = ctrl_path
         self.reward = reward
         self.env_name = env_name
         self.exp_name = exp_name
         self.gen = gen
+
     def __str__(self):
-        exp_str = f'{self.exp_name}' if self.exp_name is not None else ''
-        gen_str = f'gen{self.gen}' if self.gen is not None else ''
-        reward_str = f'({round(self.reward, 3)})' if self.reward is not None else ''
+        exp_str = f"{self.exp_name}" if self.exp_name is not None else ""
+        gen_str = f"gen{self.gen}" if self.gen is not None else ""
+        reward_str = f"({round(self.reward, 3)})" if self.reward is not None else ""
         comps = [exp_str, gen_str, reward_str]
-        out = ''
+        out = ""
         for comp in comps:
             if len(comp) != 0:
-                out += f'{comp}_'
+                out += f"{comp}_"
         return out[:-1]
-    
-class Job():
+
+
+class Job:
     def __init__(
-        self, 
-        name, 
-        experiment_names, 
-        env_names, 
+        self,
+        name,
+        experiment_names,
+        env_names,
         load_dir,
-        generations=None, 
+        generations=None,
         ranks=None,
         jobs=None,
         organize_by_jobs=True,
         organize_by_experiment=False,
-        organize_by_generation=False):
-
+        organize_by_generation=False,
+    ):
         # set values
         self.name = name
         self.experiment_names = experiment_names
@@ -166,7 +180,7 @@ class Job():
         self.generations = generations
         self.ranks = ranks
 
-        # set jobs 
+        # set jobs
         self.sub_jobs = []
         if jobs:
             for job in jobs:
@@ -174,38 +188,42 @@ class Job():
                 self.sub_jobs[-1].name = job.name if organize_by_jobs else None
         if organize_by_experiment:
             for exp_name, env_name in zip(self.experiment_names, self.env_names):
-                self.sub_jobs.append(Job(
-                    name = exp_name,
-                    experiment_names = [exp_name],
-                    env_names = [env_names],
-                    load_dir = self.load_dir,
-                    generations = self.generations,
-                    ranks = self.ranks,
-                    organize_by_experiment=False,
-                    organize_by_generation=organize_by_generation
-                ))
+                self.sub_jobs.append(
+                    Job(
+                        name=exp_name,
+                        experiment_names=[exp_name],
+                        env_names=[env_names],
+                        load_dir=self.load_dir,
+                        generations=self.generations,
+                        ranks=self.ranks,
+                        organize_by_experiment=False,
+                        organize_by_generation=organize_by_generation,
+                    )
+                )
             self.experiment_names = None
             self.env_names = None
             self.generations = None
             self.ranks = None
         elif organize_by_generation:
-            assert len(self.experiment_names) == 1, (
-                'Cannot create generation level folders for multiple experiments. Quick fix: set organize_by_experiment=True.'
-            )
-            if self.generations == None:
+            assert (
+                len(self.experiment_names) == 1
+            ), "Cannot create generation level folders for multiple experiments. Quick fix: set organize_by_experiment=True."
+            if self.generations is None:
                 exp_name = self.experiment_names[0]
                 self.generations = get_generations(self.load_dir, exp_name)
             for gen in self.generations:
-                self.sub_jobs.append(Job(
-                    name = f'generation_{gen}',
-                    experiment_names = self.experiment_names,
-                    env_names = self.env_names,
-                    load_dir = self.load_dir,
-                    generations = [gen],
-                    ranks = self.ranks,
-                    organize_by_experiment=False,
-                    organize_by_generation=False
-                ))
+                self.sub_jobs.append(
+                    Job(
+                        name=f"generation_{gen}",
+                        experiment_names=self.experiment_names,
+                        env_names=self.env_names,
+                        load_dir=self.load_dir,
+                        generations=[gen],
+                        ranks=self.ranks,
+                        organize_by_experiment=False,
+                        organize_by_generation=False,
+                    )
+                )
             self.experiment_names = None
             self.env_names = None
             self.generations = None
@@ -215,63 +233,103 @@ class Job():
         if self.name is not None and len(self.name) != 0:
             save_dir = os.path.join(save_dir, self.name)
 
-        tabs = '  '*depth
+        tabs = "  " * depth
         print(f"{tabs}\{self.name}")
-    
-        try: os.makedirs(save_dir)
-        except: pass
+
+        try:
+            os.makedirs(save_dir)
+        except:
+            pass
 
         for sub_job in self.sub_jobs:
-            sub_job.generate(load_dir, save_dir, depth+1)
+            sub_job.generate(load_dir, save_dir, depth + 1)
 
         # collect robots
-        if self.experiment_names == None:
-            return 
+        if self.experiment_names is None:
+            return
 
         robots = []
         for exp_name, env_name in zip(self.experiment_names, self.env_names):
-            exp_gens = self.generations if self.generations is not None else get_generations(self.load_dir, exp_name)
+            exp_gens = (
+                self.generations
+                if self.generations is not None
+                else get_generations(self.load_dir, exp_name)
+            )
             for gen in exp_gens:
                 for idx, reward in get_exp_gen_data(exp_name, load_dir, gen):
-                    robots.append(Robot(
-                        body_path = os.path.join(load_dir, exp_name, f"generation_{gen}", "structure", f"{idx}.npz"),
-                        ctrl_path = os.path.join(load_dir, exp_name, f"generation_{gen}", "controller", f"{idx}.zip"),
-                        reward = reward,
-                        env_name = env_name,
-                        exp_name = exp_name if len(self.experiment_names) != 1 else None,
-                        gen = gen if len(exp_gens) != 1 else None,
-                    ))
+                    robots.append(
+                        Robot(
+                            body_path=os.path.join(
+                                load_dir,
+                                exp_name,
+                                f"generation_{gen}",
+                                "structure",
+                                f"{idx}.npz",
+                            ),
+                            ctrl_path=os.path.join(
+                                load_dir,
+                                exp_name,
+                                f"generation_{gen}",
+                                "controller",
+                                f"{idx}.zip",
+                            ),
+                            reward=reward,
+                            env_name=env_name,
+                            exp_name=exp_name
+                            if len(self.experiment_names) != 1
+                            else None,
+                            gen=gen if len(exp_gens) != 1 else None,
+                        )
+                    )
 
         # sort and generate
         robots = sorted(robots, key=lambda x: x.reward, reverse=True)
-        ranks = self.ranks if self.ranks is not None else [i for i in range(len(robots))]
-        
+        ranks = (
+            self.ranks if self.ranks is not None else [i for i in range(len(robots))]
+        )
+
         # make gifs
         for i, robot in zip(ranks, robots):
             save_robot_gif(
-                os.path.join(save_dir, f'{i}_{robot}'),
+                os.path.join(save_dir, f"{i}_{robot}"),
                 robot.env_name,
                 robot.body_path,
-                robot.ctrl_path
+                robot.ctrl_path,
             )
-    
-GIF_RESOLUTION = (1280/5, 720/5)
-if __name__ == '__main__':
+
+
+GIF_RESOLUTION = (1280 / 5, 720 / 5)
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--exp_root', type=str, default='saved_data', help='Root directory for experiments')
-    parser.add_argument('--experiment_names', nargs='+', default=['test_ga'], help='List of experiment names')
-    parser.add_argument('--env_names', nargs='+', default=['Walker-v0'], help='List of environment names')
+    parser.add_argument(
+        "--exp_root",
+        type=str,
+        default="saved_data",
+        help="Root directory for experiments",
+    )
+    parser.add_argument(
+        "--experiment_names",
+        nargs="+",
+        default=["test_ga"],
+        help="List of experiment names",
+    )
+    parser.add_argument(
+        "--env_names",
+        nargs="+",
+        default=["Walker-v0"],
+        help="List of environment names",
+    )
     args = parser.parse_args()
 
     exp_root = os.path.join(args.exp_root)
-    save_dir = os.path.join(args.exp_root, 'all_media')
+    save_dir = os.path.join(args.exp_root, "all_media")
 
     my_job = Job(
-        name = 'test_ga',
-        experiment_names = args.experiment_names,
-        env_names = args.env_names,
-        ranks = [i for i in range(3)],
-        load_dir = exp_root,
+        name="test_ga",
+        experiment_names=args.experiment_names,
+        env_names=args.env_names,
+        ranks=[i for i in range(3)],
+        load_dir=exp_root,
         organize_by_experiment=False,
         organize_by_generation=True,
     )
